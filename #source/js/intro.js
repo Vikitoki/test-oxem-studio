10faddf65c1813af2133cd5336207571c140f9fb
Vasily <vasa-svistun@mail.ru>
2021-04-03 11:22:47 +0300

    Свёрстан и адаптирован блок с калькулятором

--- #source/js/intro.js
+++ #source/js/intro.js
@@ -26,51 +26,5 @@ if (document.querySelector(".slider-intro__body")) {
       clickable: true,
     },
 
-    // // Автопрокрутка
-    // autoplay: {
-    //   // Пауза между прокруткой
-    //   delay: 5000,
-    //   // Закончить на последнем слайде
-    //   stopOnLastSlide: false,
-    //   // Отключить после ручного переключения
-    //   disableOnInteraction: false,
-    // },
-
-    // // Брейкпоинты
-    // breakpoints: {
-    //   0: {
-    //     slidesPerView: 1,
-    //   },
-    //   480: {
-    //     slidesPerView: 2,
-    //   },
-    //   600: {
-    //     slidesPerView: 3,
-    //   },
-    //   992: {
-    //     slidesPerView: 4,
-    //   },
-    //   1170: {
-    //     slidesPerView: 5,
-    //   },
-    // },
   });
 }
-
-// Полезные чанки для слайдера
-
-// Get background to dots
-const itemImages = document.querySelectorAll(".item-content-intro__image"),
-  sliderIntroDotts = document.querySelectorAll(
-    ".main-intro-content__pagination .swiper-pagination-bullet"
-  );
-
-sliderIntroDotts.forEach((item, index) => {
-  let dotBackground = itemImages[index]
-    .querySelector("img")
-    .getAttribute("src");
-  item.style.cssText = `
-	background: url('${dotBackground}') center no-repeat;
-	background-size: cover;
-	`;
-});
